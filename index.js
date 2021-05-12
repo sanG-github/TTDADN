7e97f41dca4dab390331de518db1a8becaff1a03
Sang Huynh <sanghuynh20000@gmail.com>
2021-05-12 20:25:24 +0700

    OK

--- index.js
+++ index.js
@@ -1,24 +1,39 @@
-const express = require("express");
-const path = require("path");
-const axios = require("axios");
-require("dotenv").config();
+const express = require('express');
+const path = require('path');
+const axios = require('axios');
+const bodyParser = require('body-parser');
+require('dotenv').config();
 
 const app = express();
 const port = 3000;
 
-app.use(express.static(__dirname + "/public"));
+app.use(bodyParser.urlencoded({extended: false}));
 
-app.get("/getAllFeeds", (req, res) => {
+app.use(express.static(__dirname + '/public'));
+
+app.get('/getAllFeeds', (req, res) => {
 	axios
 		.get(
 			`https://io.adafruit.com/api/v2/${process.env.USER}/feeds?X-AIO-Key=${process.env.KEY}`
 		)
-		.then((res1) => res.send(res1.data))
-		.catch((err) => console.log(err));
+		.then((res1) => res.status(200).send(res1.data))
+		.catch((err) => console.log(err, 'err'));
 });
 
-app.get("/", (req, res) => {
-	res.sendFile(path.join(__dirname, "public", "index.html"));
+app.post('/changeFeed', (req, res) => {
+	axios
+		.post(
+			`https://io.adafruit.com/api/v2/${process.env.USER}/feeds/${req.body.feedName}/data?X-AIO-Key=${process.env.KEY}`,
+			{
+				datum: {value: req.body.value},
+			}
+		)
+		.then((res1) => console.log(res1))
+		.catch((err) => console.log(err, 'err'));
+});
+
+app.get('/', (req, res) => {
+	res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
 app.listen(port, () => {
--- public/scripts.js
+++ public/scripts.js
@@ -1,33 +1,44 @@
 $(document).ready(function () {
-	$(document).on("click", "#toggleFeed", function (e) {
+	function changeFeed(feedName, value) {
+		$.post('/changeFeed', {
+			value: value,
+			feedName: feedName,
+		}).then(function (res) {
+			console.log('res:', res);
+		});
+	}
+
+	$(document).on('click', '#toggleFeed', function (e) {
 		var feedName = e.target.parentElement.id;
 		var currVal = e.target.parentNode.childNodes[3];
-		var nextVal = e.target.innerText === "ON" ? "OFF" : "ON";
+		var nextVal = e.target.innerText;
 
 		currVal.innerText = nextVal;
-		console.log("toggle", feedName, nextVal);
+		changeFeed(feedName, nextVal);
+		console.log('toggle', feedName, nextVal);
 	});
 
-	$(document).on("click", "#numFeed", function (e) {
+	$(document).on('click', '#numFeed', function (e) {
 		var feedName = e.target.parentElement.id;
 		var currVal = e.target.parentNode.childNodes[3];
 		var nextVal =
 			parseFloat(currVal.innerText) +
-			(e.target.innerText === "-" ? -1 : 1);
+			(e.target.innerText === '-' ? -1 : 1);
 
 		currVal.innerText = nextVal;
-		console.log("change", feedName, nextVal);
+		changeFeed(feedName, nextVal);
+		console.log('change', feedName, nextVal);
 	});
 
-	$("#getAllFeeds").on("click", function () {
-		$.get("/getAllFeeds", function (data) {
-			var ul = document.getElementsByClassName("list-group")[0];
-			console.log(data);
+	$('#getAllFeeds').on('click', function () {
+		$.get('/getAllFeeds', function (data) {
+			var ul = document.getElementsByClassName('list-group')[0];
+
 			if (!ul.clientHeight)
 				data.map((item) => {
 					var btn = `<button class="btn btn-primary" title=${item.description}>${item.key}</button>`;
 					var childBtn =
-						(item.last_value === "ON") | "OFF"
+						item.last_value === 'ON' || item.last_value === 'OFF'
 							? `<div id=${item.key}>
 					<button type="button" class="btn btn-danger" id="toggleFeed">OFF</button>
 					<h2>${item.last_value}</h2>
@@ -39,7 +50,7 @@ $(document).ready(function () {
 					<button type="button" class="btn btn-success" id="numFeed">+</button>
 					</div>`;
 
-					var li = document.createElement("li");
+					var li = document.createElement('li');
 					li.innerHTML = btn + childBtn;
 					ul.appendChild(li);
 				});
